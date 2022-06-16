c07230ab22c5ca00aee50bdf51b7ef97262f9466
Sharonah8 <nyamongosharon88@gmail.com>
2022-06-16 14:04:01 -0700

    A function that encodes a string using rot13.

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,51 +1,29 @@
 #include "main.h"
 
 /**
- * rot13 - Encodes a string using rot13.
- * @str: The string to be encoded.
- * Return: A pointer to the encoded string.
+ * rot13 - encodes charactrs by +/- 13
+ * @n: char
+ * Return:n
  */
-char *rot13(char *str)
+
+char *rot13(char *n)
 {
-	int indx1 = 0, indx2;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-			     'G', 'H', 'I', 'J', 'K', 'L',
-     			     'M', 'N', 'O', 'P', 'Q', 'R',
-     			     'S', 'T', 'U', 'V', 'W', 'X',
-     			     'Y', 'Z', 'a', 'b', 'c', 'd',
-     			     'e', 'f', 'g', 'h', 'i', 'j',
-     			     'k', 'l', 'm', 'n', 'o', 'p',
-     			     'q', 'r', 's', 't', 'u', 'v',
-     			     'w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-			     'T', 'U', 'V', 'W', 'X', 'Y',
-     			     'Z', 'A', 'B', 'C', 'D', 'E',
-     			     'F', 'G', 'H', 'I', 'J', 'K',
-     			     'L', 'M', 'n', 'o', 'p', 'q',
-     			     'r', 's', 't', 'u', 'v', 'w',
-     			     'x', 'y', 'z', 'a', 'b', 'c',
-     			     'd', 'e', 'f', 'g', 'h', 'i',
-     			     'j', 'k', 'l', 'm'};
+	int i, j;
 
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	while (str[indx1])
+	for (i = 0; n[i] != '\0'; i++)
 	{
 
-		for (indx2 = 0; indx2 < 52; indx2++)
+		for (j = 0; a[j] != '\0'; j++)
 		{
-
-			if (str[indx1] == alphabet[indx2])
+			if (n[i] == a[j])
 			{
-
-				str[indx1] = rot13key[indx2];
+				n[i] = b[j];
 				break;
-
-																						}
+			}
 		}
-
-		indx1++;
-
 	}
-	return (str);
-
+	return (n);
 }
