91fc2dc2e2695b0b4fb1646ff76353f75294b031
Sharonah8 <nyamongosharon88@gmail.com>
2022-06-24 10:38:20 -0700

    A program that prints the minimum number of coins to make change for an amount of money.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,68 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 
 /**
- * main - prints the minimum number of coins
- * to make change for an amount of money
- * @argc: n args
- * @argv: arr args
- * Return: 0
+ * main - function
+ *@argc: length of argv
+ *@argv: number of argument
+ *Return: Always 0
  */
 int main(int argc, char *argv[])
 {
 
-	int value, c;
+	/*Declaring variables*/
+
+	int position, total, change, aux;
+
+	int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
 
 
-	c = 0;
+
+	position = total = change = aux = 0;
+
+
+
 	if (argc != 2)
+
 	{
 
 		printf("Error\n");
+
 		return (1);
 
 	}
-	value = atoi(argv[1]);
-	if (value < 0)
+
+
+
+	total = atoi(argv[1]); /*Covert str to int*/
+
+
+
+	if (total <= 0)
+
 	{
 
-		printf("%d\n", 0);
+		printf("0\n");
+
 		return (0);
 
 	}
-	if (value % 25 >= 0)
+
+
+
+	/*Declaring While*/
+
+
+
+	while (coins[position] != '\0')
+
+
+
 	{
 
-		c += value / 25;
-		value = value % 25;
+		if (total >= coins[position])
+
+		{
+
+			aux = (total / coins[position]);
+
+			change += aux;
+
+			total -= coins[position] * aux;
+
+		}
+
+
+
+		position++;
+
+
 
 	}
-	if (value % 10 >= 0)
-	{
 
-		c += value / 10;
-		value = value % 10;
 
-	}
-	if (value % 5 >= 0)
-	{
 
-		c += value / 5;
-		value = value % 5;
+	printf("%d\n", change);
 
-	}
-	if (value % 2 >= 0)
-	{
-		c += value / 2;
-		value = value % 2;
-	}
-	if (value % 1 >= 0)
-	{
-
-		c += value / 1;
-	}
-	printf("%d\n", c);
 	return (0);
+
 }
